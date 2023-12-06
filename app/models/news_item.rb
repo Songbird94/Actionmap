76ac0b6ebafe663962e5fa4dc5b1988937abd519
Xiaoru Zhao <catherine.z@berkeley.edu>
2023-12-06 02:40:44 +0000

    fix save article

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -4,6 +4,7 @@ class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
 
+
   def self.find_for(representative_id)
     NewsItem.find_by(
       representative_id: representative_id
