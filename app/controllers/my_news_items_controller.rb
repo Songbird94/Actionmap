bfdf6994a87a250cafbb1e863994635e7c70c4ad
Xiaoru Zhao <catherine.z@berkeley.edu>
2023-12-06 22:23:01 +0000

    [feat]added issue to news_items

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -6,6 +6,7 @@ class MyNewsItemsController < SessionController
   before_action :set_representative
   before_action :set_representatives_list
   before_action :set_news_item, only: %i[edit update destroy]
+  before_action :set_issues_list, only: %i[new edit create update]
 
   def new
     redirect_to search_my_news_item_path(representative_id: @representative)
@@ -15,7 +16,13 @@ class MyNewsItemsController < SessionController
   def edit; end
 
   def create
-    @news_item = NewsItem.new(news_item_params)
+    # news_item_params.each do |_index, news_item_params|
+    #   news_item_params[:representative_id] = params[:representative_id]
+    #   news_item_params[:rating] = params[:rating]
+    #   news_item_params[:issue] = params[:issue]
+    # end
+    selected_article_index = params[:article_index].to_i
+    @news_item = NewsItem.new(news_item_params[selected_article_index])
     if @news_item.save
       redirect_to representative_news_item_path(@representative, @news_item),
                   notice: 'News item was successfully created.'
@@ -45,6 +52,11 @@ class MyNewsItemsController < SessionController
 
   def search
     @representatives_list = Representative.all.map { |r| [r.name, r.id] }
+    @issues_list = ['Free Speech', 'Immigration', 'Terrorism',
+    'Social Security and Medicare', 'Abortion', 'Student Loans', 'Gun Control',
+    'Unemployment', 'Climate Change', 'Homelessness', 'Racism', 'Tax Reform',
+    'Net Neutrality', 'Religious Freedom', 'Border Security', 'Minimum Wage',
+    'Equal Pay']
   end
 
   def show_articles
@@ -90,6 +102,6 @@ class MyNewsItemsController < SessionController
 
   # Only allow a list of trusted parameters through.
   def news_item_params
-    params.require(:news_item).permit(:news, :title, :description, :link, :representative_id, :rating)
+    params.require(:news_item).permit(:news, :title, :description, :link, :representative_id, :issue, :rating)
   end
 end
